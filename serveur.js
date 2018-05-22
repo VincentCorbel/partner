2b2e8f077e71389b620fc4c764cbef80ac7d987d
vincent <vincent@me.fr>
2018-05-22 23:30:00 +0200

    package.json

--- serveur.js
+++ serveur.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var fs = require('fs')
 var app = express()
 
 app.use(bodyParser.json()) // support json encoded bodies
@@ -7,12 +8,30 @@ app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
  
 app.get('/', function (req, res) {
   res.send('Hello World')
+  
+})
+
+app.get('/lastRequestSend', function (req, res) {
+   
+   console.log('Read!');
+   fs.readFile('lastRequest.json', function(err, data) {
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+   });
+  
 })
 
 app.post('/partner', function (req, res) {
   console.log(req.body)
   var objJson =  JSON.parse(req.body.jsonStream);
   console.log(objJson.request)
+  
+  fs.writeFile('lastRequest.json', JSON.stringify(req.body), function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  
   
   if (objJson.request === "addAnomalie" ) {
 	  console.log(objJson.anomalie.id)
@@ -24,3 +43,4 @@ app.post('/partner', function (req, res) {
 })
  
 app.listen(process.env.PORT || 3000)
+
